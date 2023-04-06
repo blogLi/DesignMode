6434990f8dc390bd769f51d6c1e6cb3dfd8bcaa2
blogLiV2 <3163639404@qq.com>
2023-04-06 22:06:47 +0800

    组合模式测试

--- DesignModeDemo/src/main/java/com/design/mode/b_结构型模式/f_组合模式/MenuItem.java
+++ DesignModeDemo/src/main/java/com/design/mode/b_结构型模式/f_组合模式/MenuItem.java
@@ -5,4 +5,14 @@ package com.design.mode.b_结构型模式.f_组合模式;
  * 叶子节点（Leaf）
  */
 public class MenuItem extends MenuComponent{
+
+    public MenuItem(String name,int level) {
+        this.name=name;
+        this.level=level;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("菜单项："+name);
+    }
 }
